ec94a1f24aab91c091d43a3a0b3e3daafd522efe
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-09-16 15:26:13 +0530

    server creation

---
+++ server.js
@@ -0,0 +1,44 @@
+// server creation
+const express = require('express');
+const app = express();
+
+app.listen('5000', function() {
+    console.log('server listening on port 5000');
+});
+
+app.use(express.json()); //-> middleware function, jo bhi data get post etc hota h vo json form mei accept hota hai
+
+let user = {};
+
+//get request
+// client <- server
+app.get('/', (req, res)=> {
+    res.send('Home Page');
+});
+
+app.get('/user', (req, res) => {
+    res.json(user);
+});
+
+//post request
+// client -> server
+app.post('/user', (req, res) => {
+    user = req.body;  //-> jo data post karna hota hai vo req mei atta h
+    // console.log(req.body);
+    res.send('data has been added suceesfully');
+});
+
+//update data
+app.patch('/user', (req,res)=> {
+    let obj = req.body;
+    for(let key in obj) {
+        user[key] = obj[key];
+    }
+    res.json(user);
+});
+
+//delete
+app.delete('/user', (req,res)=> {
+    user = {};
+    res.json(user);
+});
