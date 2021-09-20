6f5c526aa2aab79659ec81149304f1fcb5e6b149
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-09-20 21:22:01 +0530

    v1

--- signup.js
+++ signup.js
@@ -1,61 +1,62 @@
-const express=require('express');
+const express = require('express');
+const { mongo } = require('mongoose');
 
-const app=express();
+const app = express();
 // const router=express.Router();
-app.listen('7000',function(){
+app.listen('7000', function () {
     console.log('server listening on port 7000');
 });
 
 app.use(express.json());
 //middleware fn
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     //do some work
     console.log('i am a middleware');
     next(); // mera kaam ho gya proceed further if nhi lgaya toh reload hota rhega
 });
 
 app.use(express.static('public')); //-> sirf public files dekhe not full code
-const userRouter=express.Router();
-const authRouter=express.Router();
+const userRouter = express.Router();
+const authRouter = express.Router();
 
-app.use('/user',userRouter);
-app.use('/auth',authRouter); 
+app.use('/user', userRouter);
+app.use('/auth', authRouter);
 //mounting in express
 
 userRouter
-.route('/')
-.get(getUser)
-.post(createUser)
-.patch(updateUser)
-.delete(deleteUser);
+    .route('/')
+    .get(getUser)
+    .post(createUser)
+    .patch(updateUser)
+    .delete(deleteUser);
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     //do some work
     console.log('i am a middleware 2nd time');
     next();
 });
 
 userRouter
-.route('/:id')
-.get(getUserById);
+    .route('/:id')
+    .get(getUserById);
 
 authRouter
-.route('/signup')
-.post(signupUser);
+    .route('/signup')
+    .post(signupUser);
 
 //Forget Password
 authRouter
-.route('/forgetPassword')
-.get(getForgetPassword)
-.post(postForgetPassword, validateEmail);
+    .route('/forgetPassword')
+    .get(getForgetPassword)
+    .post(postForgetPassword, validateEmail);
 
-function getForgetPassword(req,res){
-    res.sendFile('./public/forgetPassword.html',{root:__dirname});
+function getForgetPassword(req, res) {
+    res.sendFile('./public/forgetPassword.html', { root: __dirname });
 }
 
-function postForgetPassword(req,res,next){
-    let data=req.body;
-    console.log('data',data);
+function postForgetPassword(req, res, next) {
+    let data = req.body;
+    console.log('data', data);
     //check if email id is correct- validate
     next();
     // check if user exists in db
@@ -65,15 +66,15 @@ function postForgetPassword(req,res,next){
     // })
 };
 
-function validateEmail(req,res){
+function validateEmail(req, res) {
     console.log('in validateEmail function');
     console.log(req.body);
     //hw to check if email is correct or not -> @ , .
     //indexOf
-     res.json({
-            message:"data received",
-            data:req.body
-        });
+    res.json({
+        message: "data received",
+        data: req.body
+    });
 }
 
 
@@ -88,25 +89,45 @@ function validateEmail(req,res){
 //     res.sendFile('public/404.html',{root:__dirname})
 // });
 
+function setCreatedAt(req, res, next) {
+    let obj = req.body;
+    let length = Object.keys(obj).length;
+    if (length == 0) {
+        return res.status(400).json({ message: "cannot create user if req.body is empty" });
+    }
+    req.body.createdAt = new Date().toISOString();
+    next();
+}
+
 
 // signup page
-function signupUser(req,res){
+const userModel = require('./modals/userModal')
+function signupUser(req, res) {
     // let userDetails=req.body;
     // let name=userDetails.name;
     // let email=userDetails.email;
     // let password=userDetails.password;
 
-    let{email,name,password}=req.body;
-    user.push({email,name,password});
-    console.log('user',req.body);
-    res.json({
-        message:'user signedUp',
-        user:req.body
-    });
+    // let{email,name,password}=req.body;
+    // user.push({email,name,password}); 
+    // Now we learn to add data in mongoDb
+    try {
+        let userObj = req.body;
+        let user = await userModal.create(userObj);
+        // console.log('user',req.body);
+        res.json({
+            message: 'user signedUp',
+            user: userObj
+        });
+    } catch (err) {
+        console.log(err);
+        res.json({ message: err.message })
+    }
+
 }
 
 
-let user=[];
+let user = [];
 // client <- server
 //crud- create read update delete
 //read
@@ -116,7 +137,7 @@ let user=[];
 
 // app.get('/user',getUser);
 
-function getUser(req,res){
+function getUser(req, res) {
     console.log('getUser called');
     res.json(user);
 }
@@ -125,30 +146,30 @@ function getUser(req,res){
 // client-> server 
 //create
 // app.post('/user',createUser);
-function createUser(req,res){
-    user=req.body;
+function createUser(req, res) {
+    user = req.body;
     // console.log(req.body);
     res.send('data has been added succesfully');
 }
 //update
 // app.patch('/user',updateUser);
-function updateUser (req,res){
-    let obj=req.body;
-    for(let key in obj){
-        user[key]=obj[key];
+function updateUser(req, res) {
+    let obj = req.body;
+    for (let key in obj) {
+        user[key] = obj[key];
     }
     res.json(user);
 };
 //delete 
 // app.delete('/user',deleteUser);
-function deleteUser(req,res){
-    user={};
+function deleteUser(req, res) {
+    user = {};
     res.json(user);
     // res.send('ussr has been deleted');
 }
 //param route
 // app.get('/user/:id',getUserById);
-function getUserById(req,res){
+function getUserById(req, res) {
     console.log(req.params);
     res.json(req.params.id);
 }
