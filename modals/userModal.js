dcb55db20e67d00363c44bbcf1407284f405c067
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-09-21 15:35:43 +0530

    modal

---
+++ modals/userModal.js
@@ -0,0 +1,76 @@
+const mongoose = require('mongoose');
+
+//mongoDB connection
+// get link from mongoDb and replace <password> with your password
+const {db_link} = require('../secret'); 
+var validator = require("email-validator");
+
+mongoose.connect(db_link).then(function(db) {
+    // console.log(db);
+    console.log('db connected');
+})
+.catch(function(err) {
+    console.log(err);
+})
+
+// schema creation
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number
+    },
+    createdAt: {
+        type: String,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: function() {
+            return validator.validate(this.email);
+        }
+    },
+    createdAt: {
+        type: Date
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 8
+    },
+    confirmPassword: {
+        type: String,
+        required: true,
+        min: 8,
+        validate: function() {
+            return this.password == this.confirmPassword;
+        }
+    },
+});
+
+// js pre hook
+// an confirm pswd chala jyega mongodb se as it cant be diff from pswd 
+userSchema.pre('save', function(){
+    this.confirmPassword = undefined;
+});
+
+//modal creation
+const userModal = mongoose.model('userModal', userSchema);
+
+module.exports = userModel;
+
+(async function createUser() {
+    let user = {
+        name: 'Japneet',
+        age: 20,
+        email: 'qaz@gmail.com',
+        password: '12345678',
+        confirmPassword: '12345678'
+    };
+
+    // create collection
+    console.log(userObj);
+})(); // -> made immediate invoke function
