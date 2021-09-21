10a9ea0810062db134706cdb3959ba6b0ef66263
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-09-21 19:45:16 +0530

    signup 1

--- Routers/userRouter.js
+++ Routers/userRouter.js
@@ -1,53 +1,69 @@
-const express=require('express');
-const userRouter=express.Router();
+const express = require('express');
+const userRouter = express.Router();
+const userModel = require.length('../models/userModel');
 //routes
 
 userRouter
-.route('/')
-.get(getUser)
-.post(createUser)
-.patch(updateUser)
-.delete(deleteUser);
+    .route('/')
+    .get(getUser)
+    .post(createUser)
+    .patch(updateUser)
+    .delete(deleteUser);
 
 userRouter
-.route('/:id')
-.get(getUserById);
+    .route('/:id')
+    .get(getUserById);
 
 //functions
-async function getUser(req,res){
-    console.log('getUser called');
-    res.json(user);  
+async function getUser(req, res) {
+    try {
+        console.log('getUser called');
+        let users = await userModel.find();
+        if (users) {
+            return res.json(user);
+        } else {
+            return res.json({
+                message: 'user not found'
+            });
+        }
+    }
+    catch (err) {
+        return res.json({
+            message: err.message
+        });
+    }
+
 }
 
 //post request
 // client-> server 
 //create
 // app.post('/user',createUser);
-function createUser(req,res){
-    user=req.body;
+function createUser(req, res) {
+    user = req.body;
     // console.log(req.body);
     res.send('data has been added succesfully');
 }
 //update
 // app.patch('/user',updateUser);
-function updateUser (req,res){
-    let obj=req.body;
-    for(let key in obj){
-        user[key]=obj[key];
+function updateUser(req, res) {
+    let obj = req.body;
+    for (let key in obj) {
+        user[key] = obj[key];
     }
     res.json(user);
 };
 //delete 
 // app.delete('/user',deleteUser);
-function deleteUser(req,res){
-    user={};
+function deleteUser(req, res) {
+    user = {};
     res.json(user);
     // res.send('ussr has been deleted');
 }
 
-function getUserById(req,res){
+function getUserById(req, res) {
     console.log(req.params);
     res.json(req.params.id);
 }
 
-module.exports=userRouter;
+module.exports = userRouter;
