2854709468a7c05b62936732f887837a0a923451
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-09-21 21:03:49 +0530

    signup final

--- server.js
+++ server.js
@@ -1,56 +1,57 @@
-// server creation
-const express = require('express');
-const app = express();
+const express=require('express');
 
-app.listen('5000', function() {
+const app=express();
+// const router=express.Router();
+app.listen('5000',function(){
     console.log('server listening on port 5000');
 });
 
-app.use(express.json()); //-> middleware function, jo bhi data get post etc hota h vo json form mei accept hota hai
+app.use(express.json());
+// app.use((req,res,next)=>{
+//     //do some work
+//     console.log('i am a middleware');
+//     next();
+// });
 
-let user = {};
+app.use(express.static('public'));
+const userRouter=require('./Routers/userRouter');
+const authRouter=require('./Routers/authRouter');
 
-//crud : create read update delete
+app.use('/user',userRouter);
+app.use('/auth',authRouter);
+//mounting in express
 
-//get request
+//param route
+// app.get('/user/:id',getUserById);
+
+
+// app.use((req,res,next)=>{
+//     //do some work
+//     console.log('i am a middleware 2nd time');
+//     next();
+// });
+
+
+
+
+
+//redirects
+// app.get('/user-all',(req,res)=>{
+//     res.redirect('/user');
+// });
+
+// //404 page
+// app.use((req,res)=>{
+//     res.sendFile('public/404.html',{root:__dirname})
+// });
+
+// let user=[];
 // client <- server
+//crud- create read update delete
 //read
-app.get('/', (req, res)=> {
-    res.send('Home Page');
-});
+// app.get('/',(req,res)=>{
+//     res.send('Home Page');
+// });
 
-app.get('/user', (req, res) => {
-    res.json(user);
-});
+// app.get('/user',getUser);
 
-//post request
-// client -> server
-//create
-app.post('/user', (req, res) => {
-    user = req.body;  //-> jo data post karna hota hai vo req mei atta h
-    // console.log(req.body);
-    res.send('data has been added suceesfully');
-});
-
-//update 
-app.patch('/user', (req,res)=> {
-    let obj = req.body;
-    for(let key in obj) {
-        user[key] = obj[key];
-    }
-    res.json(user);
-});
-
-//delete
-app.delete('/user', (req,res)=> {
-    user = {};
-    res.json(user);
-});
-
-
-//parameter route
-app.get('/user/:id',(req, res)=> {
-    console.log(req.params);
-    // res.send(req.params.id);
-    res.json(req.params.id);
-});
